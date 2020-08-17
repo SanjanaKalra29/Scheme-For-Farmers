718cbd91772b34e8d612c9c8ac142d6e25249891
shivam2050 <34658091+shivam2050@users.noreply.github.com>
2020-08-17 11:02:56 +0530

    user added

---
+++ src/main/java/com/lti/entity/User.java
@@ -0,0 +1,35 @@
+package com.lti.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TBL_USER")
+public class User {
+	@Id
+	private int id;
+	
+	private String First_name;
+	private String Last_name;
+	
+	private String role;
+	private String Dob;
+	private String Email;
+	private int Phone_no;
+	private String Gender;
+	private String Address;
+	private String Aadhar_card;
+	
+	private String Username;
+	private String Password;
+	private String Pancard;
+ 
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+
+}
